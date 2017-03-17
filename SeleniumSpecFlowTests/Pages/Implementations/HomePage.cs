fc7f00c8b4ac9b64d6d1b8a6059ed875c7c62c69
Catalin Lungu <C.Lungu@res.com>
2017-03-17 17:21:42 +0200

    changed a homepage definition

--- SeleniumSpecFlowTests/Pages/Implementations/HomePage.cs
+++ SeleniumSpecFlowTests/Pages/Implementations/HomePage.cs
@@ -5,25 +5,13 @@ namespace SeleniumSpecFlowTests.Pages.Implementations
     public class HomePage : BasePage
     {
         const string idMenu = "side-menu";
-        private IWebElement ServiceCatalogMenuItem => Driver.FindElementWhenVisible(By.XPath($"//*[@id='{idMenu}']/li[2]/a"));
-        //  
-        //     public void NavigateToServiceCatalogPage()
-        //     {
-        //         Driver.FindElementWhenVisible(By.CssSelector("body > nav.topBar .topBarLeft")).Click();
-        //         Driver.FindElementWhenVisible(By.CssSelector("body > nav.topBar .topBarLeft .dropdown-menu li:nth-child(3)")).Click();
-        //     }
+        const string name = "Service Catalog";
+        private IWebElement ServiceCatalogMenuItem => Driver.FindElementWhenVisible(By.XPath($"//a[contains(text(), '{name}')]"));
 
+        //private IWebElement ServiceCatalogMenuItem => Driver.FindElementWhenVisible(By.XPath($"//*[@id='{idMenu}']/li[2]/a"));
         public void GoToServiceCatalog()
         {
             ServiceCatalogMenuItem.Click();
         }
-
-        public void OpenServiceFromOverviewPage(string name)
-        {
-            IWebElement element = WebDriver.Instance().FindElementWhenVisible(By.XPath($"//a[contains(text(), '{name}')]"));
-            ScrollIntoView(element);
-            element.Click();
-        }
-
     }
 }
