2799047a19d76d2dc7af11bc1d0767a1b9d1b97d
Catalin Lungu <C.Lungu@res.com>
2017-03-20 19:59:58 +0200

    additional changes and test rail package

---
+++ SeleniumSpecFlowTests/Pages/Common/Menu.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumSpecFlowTests.Pages.Implementations
+{
+    public class Menu : BasePage
+    {
+        const string idMenu = "side-menu";
+        const string CatalogString = "Service Catalog";
+        const string Home = "Dashboard";
+        //private IWebElement ServiceCatalogMenuItem => Driver.FindElementWhenVisible(By.XPath($"//a[contains(text(), '{name}')]"));
+        //private IWebElement ServiceCatalogMenuItem => Driver.FindElementWhenVisible(By.XPath($"//*[@id='{idMenu}']/li[2]/a"));
+        public static void GoToServiceCatalog()
+        {
+            //ServiceCatalogMenuItem.Click();
+            WebDriver.Instance().FindElementWhenClickable(By.XPath($"//a[contains(text(), '{CatalogString}')]")).Click();
+        }
+        public static void GoToHomePage()
+        {
+            //ServiceCatalogMenuItem.Click();
+            WebDriver.Instance().FindElementWhenClickable(By.XPath($"//a[contains(text(), '{Home}')]")).Click();
+        }
+
+    }
+}
